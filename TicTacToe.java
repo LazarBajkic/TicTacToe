250ba4bd903c92f0d3b2905e26dfe11e56f12e94
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-03-04 12:43:31 +0100

    Functionality begin
    Began the making of the programs functionality

--- TicTacToe.java
+++ TicTacToe.java
@@ -26,6 +26,12 @@ public class TicTacToe extends JFrame implements MouseListener{
     JLabel row2=new JLabel();
     JLabel column1=new JLabel();
     JLabel column2=new JLabel();
+    JLabel score = new JLabel();
+    
+    int turnCounter=0;
+    int checkRowX=0;
+    int checkColumnX=0;
+    int checkDiagonalX=0;
 
     TicTacToe(){
         
@@ -56,37 +62,45 @@ public class TicTacToe extends JFrame implements MouseListener{
 
         square1.setSize(100, 100);
         square1.setLocation(0,0);
-        square1.setIcon(O);
         square1.addMouseListener(this);
 
         square2.setSize(100,100);
         square2.setLocation(120,0);
+        square2.addMouseListener(this);
 
         square3.setSize(100,100);
         square3.setLocation(220,0);
+        square3.addMouseListener(this);
 
         square4.setSize(100,100);
         square4.setLocation(0,120);
+        square4.addMouseListener(this);
 
         square5.setSize(100,100);
         square5.setLocation(120,120);
-        square5.setIcon(O);
+        square5.addMouseListener(this);
 
         square6.setSize(100,100);
         square6.setLocation(220,120);
+        square6.addMouseListener(this);
 
         square7.setSize(100,100);
         square7.setLocation(0,240);
-        square7.setIcon(O);
+        square7.addMouseListener(this);
 
         square8.setSize(100,100);
         square8.setLocation(120,240);
-        square8.setIcon(O);
+        square8.addMouseListener(this);
 
         square9.setSize(100,100);
         square9.setLocation(240,240);
-        square9.setIcon(O);
+        square9.addMouseListener(this);
 
+        score.setSize(200, 30);
+        score.setLocation(100, 320);
+        score.setText("Turn number: "+turnCounter);
+
+        this.add(score);
         this.add(column1);
         this.add(column2);
         this.add(row1);
@@ -105,32 +119,96 @@ public class TicTacToe extends JFrame implements MouseListener{
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+        
+        if(e.getSource()==square1&&turnCounter % 2 == 0){
+            square1.setIcon(O);
+           }
+           else if(e.getSource()==square1&&turnCounter % 2 == 1){
+            square1.setIcon(X);
+           }
+
+           if(e.getSource()==square2&&turnCounter % 2 == 0){
+            square2.setIcon(O);
+           }
+           else if(e.getSource()==square2&&turnCounter % 2 == 1){
+            square2.setIcon(X);
+           }
+
+           if(e.getSource()==square3&&turnCounter % 2 == 0){
+            square3.setIcon(O);
+           }
+           else if(e.getSource()==square3&&turnCounter % 2 == 1){
+            square3.setIcon(X);
+           }
+
+           if(e.getSource()==square4&&turnCounter % 2 == 0){
+            square4.setIcon(O);
+           }
+           else if(e.getSource()==square4&&turnCounter % 2 == 1){
+            square4.setIcon(X);
+           }          
+
+           if(e.getSource()==square5&&turnCounter % 2 == 0){
+            square5.setIcon(O);
+           }
+           else if(e.getSource()==square5&&turnCounter % 2 == 1){
+            square5.setIcon(X);
+           }    
+
+           if(e.getSource()==square6&&turnCounter % 2 == 0){
+            square6.setIcon(O);
+           }
+           else if(e.getSource()==square6&&turnCounter % 2 == 1){
+            square6.setIcon(X);
+           }
+
+           if(e.getSource()==square7&&turnCounter % 2 == 0){
+            square7.setIcon(O);
+           }
+           else if(e.getSource()==square7&&turnCounter % 2 == 1){
+            square7.setIcon(X);
+           }
+
+           if(e.getSource()==square8&&turnCounter % 2 == 0){
+            square8.setIcon(O);
+           }
+           else if(e.getSource()==square8&&turnCounter % 2 == 1){
+            square8.setIcon(X);
+           }
+
+           if(e.getSource()==square9&&turnCounter % 2 == 0){
+            square9.setIcon(O);
+           }
+           else if(e.getSource()==square9&&turnCounter % 2 == 1){
+            square9.setIcon(X);
+           }
+
+           turnCounter=turnCounter+1;
+           score.setText("Turn number: "+turnCounter);
+
+           throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+      
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+           
     }
 
 }
